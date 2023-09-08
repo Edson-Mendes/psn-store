d1cd7d52279c8a5e9d6bdfa0e98becece4c6efd0
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-09-08 14:56:15 -0300

    feat: add componente card

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-card-pricing',
-  templateUrl: './card-pricing.component.html',
-  styleUrls: ['./card-pricing.component.css']
+	selector: 'app-card-pricing',
+	templateUrl: './card-pricing.component.html',
+	styleUrls: ['./card-pricing.component.css'],
 })
 export class CardPricingComponent {
-
+	@Input() gameType: string = 'Digital PS4';
+	@Input() gamePrice: string = 'R$ 399,90';
 }

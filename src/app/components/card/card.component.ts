0fbad2157dbdffabd16e7eda461b3f5890a22dba
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-09-08 16:17:02 -0300

    feat: layout concluído

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+	selector: 'app-card',
+	templateUrl: './card.component.html',
+	styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-
+	@Input() gameCoverURL: string = '';
+	@Input() gameLabel: string = 'EXCLUSIVE';
+	@Input() gameType: string = 'XPTO PS4';
+	@Input() gamePrice: string = 'R$ 159,90';
 }
